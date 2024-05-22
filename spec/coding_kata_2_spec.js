22b5b43841802d012a19e203456f05ca5537898c
Ron <if22b080@technikum-wien.at>
2024-05-22 22:13:53 +0200

    Red: add failing test

--- spec/coding_kata_2_spec.js
+++ spec/coding_kata_2_spec.js
@@ -12,4 +12,9 @@ describe('Coding Kata 2', function () {
         expect(electrons.solve(2, 2, 3, 3))
             .toEqual(4);
     });
+
+    it('Should return 2 for given array', function() {
+        expect(electrons.solve(6, 6, 4, 4, 1, 3))
+            .toEqual(2);
+    });
 });
